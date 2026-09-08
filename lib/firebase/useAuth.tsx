888744ea895ae6c84c2dfe063f816e16b8705ff8
Tamilvendhan <tamilvendhan@example.com>
2026-09-08 23:59:46 +0530

    Fix disabled Firebase auth type narrowing

--- lib/firebase/useAuth.tsx
+++ lib/firebase/useAuth.tsx
@@ -1,13 +1,4 @@
 "use client";
-import { useEffect, useState } from 'react';
-import { firebaseAuth } from './init';
-
 export function useAuth() {
-  const [user, setUser] = useState<any>(null);
-
-  useEffect(() => {
-    if (firebaseAuth) setUser(firebaseAuth.currentUser);
-  }, []);
-
-  return { user };
+  return { user: null };
 }
